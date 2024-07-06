0b046050fc79b474efbed38a95a9727056fb26cb
KepinTheNoob <145685209+KepinTheNoob@users.noreply.github.com>
2024-07-06 20:22:30 +0700

    [BNCC-48] Get profile field options for region, faculty, & major

--- src/routes/profileRouter.ts
+++ src/routes/profileRouter.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { createProfile, login, register } from "../controllers";
+import { createProfile, login, register, getRegions, getFaculties, getMajors } from "../controllers";
 import { authenticationMiddleware } from "../middleware";
 
 const profileRouter = express.Router();
 
+profileRouter.get("/options/regions", getRegions);
+profileRouter.get("/options/regions/:regionId/faculties", getFaculties);
+profileRouter.get("/options/regions/:regionId/faculties/:facultyId/majors", getMajors);
 profileRouter.use(authenticationMiddleware);
 
 profileRouter.post("/", createProfile);
