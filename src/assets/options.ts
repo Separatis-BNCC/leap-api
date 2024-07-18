9fb14eade6af40445cac636e3b4ef65be955dd83
KepinTheNoob <145685209+KepinTheNoob@users.noreply.github.com>
2024-07-18 21:03:45 +0700

    [BNCC-48] Add string array

--- src/assets/options.ts
+++ src/assets/options.ts
@@ -471,230 +471,70 @@ export default {
   ],
 };
 
-// SOCS
-// {
-//   id: 1,
-//   label: "Artificial Intellegence",
-// },
-// {
-//   id: 2,
-//   label: "Cyber Security",
-// },
-// {
-//   id: 3,
-//   label: "Game Application and Technology",
-// },
-// {
-//   id: 4,
-//   label: "Computer Science",
-// },
-// {
-//   id: 5,
-//   label: "Data Science",
-// },
+// const Majors = [
+//   "Artificial Intellegence",
+//   "Cyber Security",
+//   "Game Application and Technology",
+//   "Computer Science",
+//   "Data Science",
+//   "Business Analytics",
+//   "Information Systems Accounting and Auditing",
+//   "Information Systems",
+//   "Interior Design",
+//   "Visual Communication Design - Animation",
+//   "Visual Communication Design - Creative Advertising",
+//   "Visual Communication Design - New Media",
+//   "Film",
+//   "Fashion",
+//   "Visual Communication Design",
+//   "Business Creation",
+//   "Global Business Marketing",
+//   "International Business Management",
+//   "Management",
+//   "Creativepreneurship",
+//   "Entrepreneurship - Business Creation",
+//   "Accounting",
+//   "Taxation",
+//   "Finance",
+//   "Communication - Mass Communication",
+//   "Communication - Marketing Communication",
+//   "Hotel Management",
+//   "Tourism",
+//   "Communication",
+//   "Public Relations",
+//   "Law - Business Law",
+//   "Psychology",
+//   "Chinese Literature",
+//   "Japanese Literature",
+//   "English Literature",
+//   "International Relations",
+//   "Primary Teacher Education",
+//   "Architecture",
+//   "Civil Engineering",
+//   "Industrial Engineering",
+//   "Computer Engineering",
+//   "Biotechnology",
+//   "Food Technology",
+//   "Computer Science and Mathematics",
+//   "Computer Science and Statistics",
+//   "Accounting and Information Systems",
+//   "Management and Information Systems",
+//   "Interactive Design and Technology",
+//   "Digital Business Innovation",
+//   "Master of Information Systems Management",
+//   "Master of Information Technology",
+//   "Master of Management",
+// ]
 
-// SOIS
-// {
-//   id: 6,
-//   label: "Business Analytics",
-// },
-// {
-//   id: 7,
-//   label: "Information Systems Accounting and Auditing",
-// },
-// {
-//   id: 8,
-//   label: "Information Systems",
-// },
-
-// SOD (School of Design)
-// {
-//   id: 9,
-//   label: "Interior Design",
-// },
-// {
-//   id: 10,
-//   label: "Visual Communication Design - Animation",
-// },
-// {
-//   id: 11,
-//   label: "Visual Communication Design - Creative Advertising",
-// },
-// {
-//   id: 12,
-//   label: "Visual Communication Design - New Media",
-// },
-// {
-//   id: 13,
-//   label: "Film",
-// },
-// {
-//   id: 14,
-//   label: "Fashion",
-// },
-// {
-//   id: 15,
-//   label: "Visual Communication Design",
-// },
-
-// BINUS Business School Undergraduate Programs
-// {
-//   id: 16,
-//   label: "Business Creation",
-// },
-// {
-//   id: 17,
-//   label: "Global Business Marketing",
-// },
-// {
-//   id: 18,
-//   label: "International Business Management",
-// },
-// {
-//   id: 19,
-//   label: "Management",
-// },
-// {
-//   id: 20,
-//   label: "Creativepreneurship",
-// },
-// {
-//   id: 21,
-//   label: "Entrepreneurship - Business Creation",
-// },
-
-// School of Accounting
-// {
-//   id: 22,
-//   label: "Accounting",
-// },
-// {
-//   id: 23,
-//   label: "Taxation",
-// },
-// {
-//   id: 24,
-//   label: "Finance",
-// },
-
-// Faculty of Digital Communication and Hotel & Tourism
-// {
-//   id: 25,
-//   label: "Communication - Mass Communication",
-// },
-// {
-//   id: 26,
-//   label: "Communication - Marketing Communication",
-// },
-// {
-//   id: 27,
-//   label: "Hotel Management",
-// },
-// {
-//   id: 28,
-//   label: "Tourism",
-// },
-// {
-//   id: 29,
-//   label: "Communication",
-// },
-// {
-//   id: 30,
-//   label: "Public Relations",
-// },
-
-// Faculty of Humanities
-// {
-//   id: 31,
-//   label: "Law - Business Law",
-// },
-// {
-//   id: 32,
-//   label: "Psychology",
-// },
-// {
-//   id: 33,
-//   label: "Chinese Literature",
-// },
-// {
-//   id: 34,
-//   label: "Japanese Literature",
-// },
-// {
-//   id: 35,
-//   label: "English Literature",
-// },
-// {
-//   id: 36,
-//   label: "International Relations",
-// },
-// {
-//   id: 37,
-//   label: "Primary Teacher Education",
-// },
-
-// Faculty of Engineering
-// {
-//   id: 38,
-//   label: "Architecture",
-// },
-// {
-//   id: 39,
-//   label: "Civil Engineering",
-// },
-// {
-//   id: 40,
-//   label: "Industrial Engineering",
-// },
-// {
-//   id: 41,
-//   label: "Computer Engineering",
-// },
-// {
-//   id: 42,
-//   label: "Biotechnology",
-// },
-// {
-//   id: 43,
-//   label: "Food Technology",
-// },
-
-// Double Program
-// {
-//   id: 44,
-//   label: "Computer Science and Mathematics",
-// },
-// {
-//   id: 45,
-//   label: "Computer Science and Statistics",
-// },
-// {
-//   id: 46,
-//   label: "Accounting and Information Systems",
-// },
-// {
-//   id: 47,
-//   label: "Management and Information Systems",
-// },
-// {
-//   id: 48,
-//   label: "Interactive Design and Technology",
-// },
-// {
-//   id: 49,
-//   label: "Digital Business Innovation",
-// },
-
-// Master Track Programs
-// {
-//   id: 50,
-//   label: "Master of Information Systems Management",
-// },
-// {
-//   id: 51,
-//   label: "Master of Information Technology",
-// },
-// {
-//   id: 52,
-//   label: "Master of Management",
-// },
+// const Faculties = [
+//   "SOCS",
+//   "SOIS",
+//   "SOD (School of Design)",
+//   "BINUS Business School Undergraduate Programs",
+//   "School of Accounting",
+//   "Faculty of Digital Communication and Hotel & Tourism",
+//   "Faculty of Humanities",
+//   "Faculty of Engineering",
+//   "Double Program",
+//   "Master Track Programs",
+// ]
