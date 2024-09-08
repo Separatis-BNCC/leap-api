5a7d03f62e50422ea934d24aa2cb07b8c6dff1e5
KepinTheNoob <145685209+KepinTheNoob@users.noreply.github.com>
2024-09-08 22:28:27 +0700

    [SCRUM-141] Edit Member Profile

--- src/controllers/memberDashboardController.ts
+++ src/controllers/memberDashboardController.ts
@@ -1,10 +1,11 @@
 import { RequestHandler } from "express";
-import {
-  errNotFound,
+import { 
+  errBadRequest,
   errInternalServer,
-  successRes,
+  successRes, 
 } from "../utils";
 import { Credential, Profile } from "../models";
+import { ValidationError } from "sequelize";
 
 export const getMemberProfile: RequestHandler = async (req, res, next) => {
   try {
@@ -22,12 +23,46 @@ export const getMemberProfile: RequestHandler = async (req, res, next) => {
         model: Profile,
         attributes: {
           exclude: ["createdAt", "updatedAt"],
-        }
-      }
+        },
+      },
     });
 
     return successRes(res, data);
   } catch (err: any) {
+    return errInternalServer(next);
+  }
+};
+
+export const editMemberProfile: RequestHandler = async (req, res, next) => {
+  try {
+    type UpdatedFields = {
+      first_name?: string;
+      last_name?: string;
+      nim?: string;
+      birth_date?: Date;
+      region?: number;
+      faculty?: number;
+      major?: number;
+      line_id?: string;
+    };
+
+    const user = JSON.parse(req.headers.user as string);
+    const payload = req.body as UpdatedFields;
+    
+    const data = await Profile.update(payload, {
+      where: {
+        id: user.id,
+      },
+    });
+
+    return successRes(res, `Success edit profile with id ${user.id}!`);
+  } catch (err: any) {
+    if ((err.name = "SequelizeValidationError")) {
+      const errors = err.errors?.map((error: ValidationError) => error.message);
+
+      return errBadRequest(next, errors);
+    }
+
     return errInternalServer(next);
   }
 };
